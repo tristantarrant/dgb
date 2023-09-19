4f0e6941d201062b746e5ccbcc2d4c53e3aed500
aleoncini <andrea.leoncini@gmail.com>
2023-09-19 15:04:10 +0200

    ns

--- src/main/java/it/redhat/dgb/model/DgbSchema.java
+++ src/main/java/it/redhat/dgb/model/DgbSchema.java
@@ -6,6 +6,6 @@ import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
 @AutoProtoSchemaBuilder(includeClasses = SftRec.class,
                         schemaPackageName = "it.redhat.dgb",
                         schemaFilePath = "proto",
-                        schemaFileName = "dgb")
+                        schemaFileName = "dgb.proto")
 public interface DgbSchema extends GeneratedSchema{
 }
